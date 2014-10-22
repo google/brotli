d3369ea96c69cc3ba9bff0677232c1dde0eb8b3e
r-lyeh <rlyeh.nospam@gmail.com>
2014-10-22 17:05:26 +0200

    deprecated rand_r() calls removed(); log() disambiguation; missing assert() header added; portable endian.h header added (for visual studio);

--- enc/write_bits.h
+++ enc/write_bits.h
@@ -18,15 +18,11 @@
 #define BROTLI_ENC_WRITE_BITS_H_
 
 #include <assert.h>
-#if defined(OS_MACOSX)
-  #include <machine/endian.h>
-#else
-  #include <endian.h>
-#endif
 #include <stdint.h>
 #include <stdio.h>
 
 #include "./port.h"
+#include "./endian.h"
 
 namespace brotli {
 
