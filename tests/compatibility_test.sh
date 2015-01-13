1d76397891e496d1bb46f1ad3f024c02beee0dce
Zoltan Szabadka <szabadka@google.com>
2015-01-13 16:42:35 +0100

    Add more test cases.

--- tests/compatibility_test.sh
+++ tests/compatibility_test.sh
@@ -7,6 +7,19 @@ set -o errexit
 
 BRO=../tools/bro
 INPUTS="""
+testdata/empty.compressed
+testdata/x.compressed
+testdata/64x.compressed
+testdata/10x10y.compressed
+testdata/xyzzy.compressed
+testdata/quickfox.compressed
+testdata/ukkonooa.compressed
+testdata/monkey.compressed
+testdata/backward65536.compressed
+testdata/zeros.compressed
+testdata/quickfox_repeated.compressed
+testdata/compressed_file.compressed
+testdata/compressed_repeated.compressed
 testdata/alice29.txt.compressed
 testdata/asyoulik.txt.compressed
 testdata/lcet10.txt.compressed
