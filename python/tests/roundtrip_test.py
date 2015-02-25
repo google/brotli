cb3826a18cfd0069ae9c7f0db31d3adb73340f7a
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-02-25 19:03:24 +0000

    moved Python test scripts to python/tests subfolder

--- python/tests/roundtrip_test.py
+++ python/tests/roundtrip_test.py
@@ -16,34 +16,33 @@ def diff_q(first_file, second_file):
 
 BRO = os.path.abspath(os.path.join(".", "bro.py"))
 
-INPUTS = [
-    os.path.join("..", "tests", "testdata", "alice29.txt"),
-    os.path.join("..", "tests", "testdata", "asyoulik.txt"),
-    os.path.join("..", "tests", "testdata", "lcet10.txt"),
-    os.path.join("..", "tests", "testdata", "plrabn12.txt"),
-    os.path.join("..", "enc", "encode.cc"),
-    os.path.join("..", "enc", "dictionary.h"),
-    os.path.join("..", "dec", "decode.c"),
-    BRO
-]
+INPUTS = """\
+testdata/alice29.txt
+testdata/asyoulik.txt
+testdata/lcet10.txt
+testdata/plrabn12.txt
+../enc/encode.cc
+../enc/dictionary.h
+../dec/decode.c
+%s
+""" % BRO
 
-
-for filename in INPUTS:
+os.chdir(os.path.abspath(os.path.join("..", "..", "tests")))
+for filename in INPUTS.splitlines():
     filename = os.path.abspath(filename)
     print('Roundtrip testing of file "%s"' % os.path.basename(filename))
     compressed = os.path.splitext(filename)[0] + ".bro"
     uncompressed = os.path.splitext(filename)[0] + ".unbro"
     with open(filename, "rb") as infile:
         data = infile.read()
-        bufsize = len(data)
     call('"%s" -f -i "%s" -o "%s"' % (BRO, filename, compressed), shell=True)
-    call('"%s" -f -d -i "%s" -o "%s" -b %d' %
-         (BRO, compressed, uncompressed, bufsize), shell=True)
+    call('"%s" -f -d -i "%s" -o "%s"' %
+         (BRO, compressed, uncompressed), shell=True)
     if diff_q(filename, uncompressed) != 0:
         sys.exit(1)
     # Test the streaming version
-    p = Popen("%s | %s -d -b %d > %s" % (BRO, BRO, bufsize, uncompressed), 
-              shell=True, stdin=PIPE)
+    p = Popen("%s | %s -d > %s" % (BRO, BRO, uncompressed), stdin=PIPE,
+              shell=True)
     p.communicate(data)
     if diff_q(filename, uncompressed) != 0:
         sys.exit(1)
