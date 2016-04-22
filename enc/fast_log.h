762b76ba8e2c99d4fb41f44c957ca10aea5d69f5
Frank Aurich <f.aurich@technisat.de>
2016-04-22 10:42:01 +0200

    Fix compilation with VS2012

--- enc/fast_log.h
+++ enc/fast_log.h
@@ -124,7 +124,7 @@ static inline double FastLog2(size_t v) {
   if (v < sizeof(kLog2Table) / sizeof(kLog2Table[0])) {
     return kLog2Table[v];
   }
-#if defined(_MSC_VER) && _MSC_VER <= 1600
+#if defined(_MSC_VER) && _MSC_VER <= 1700
   // Visual Studio 2010 does not have the log2() function defined, so we use
   // log() and a multiplication instead.
   static const double kLog2Inv = 1.4426950408889634f;
