a260b6ba73e0b0eab6d7e7385dbaa476d36f30ab
Alex Nicksay <nicksay@gmail.com>
2016-10-31 08:24:01 -0400

    Python: Add tests for streamed compression (#458)
    Progress on #191

--- python/tests/test_utils.py
+++ python/tests/test_utils.py
@@ -8,29 +8,32 @@ import filecmp
 def diff_q(first_file, second_file):
     """Simulate call to POSIX diff with -q argument"""
     if not filecmp.cmp(first_file, second_file, shallow=False):
-        print("Files %s and %s differ" % (first_file, second_file),
-              file=sys.stderr)
+        print(
+            'Files %s and %s differ' % (first_file, second_file),
+            file=sys.stderr)
         return 1
     return 0
 
 
-PYTHON = sys.executable or "python"
+project_dir = os.path.abspath(os.path.join(__file__, '..', '..', '..'))
 
-# 'bro.py' script should be in parent directory
-BRO = os.path.abspath("../bro.py")
+PYTHON = sys.executable or 'python'
 
-# get platform- and version-specific build/lib folder
-platform_lib_name = "lib.{platform}-{version[0]}.{version[1]}".format(
-    platform=sysconfig.get_platform(),
-    version=sys.version_info)
+BRO = os.path.join(project_dir, 'python', 'bro.py')
 
-# by default, distutils' build base is in the same location as setup.py
-build_base = os.path.abspath(os.path.join("..", "..", "bin"))
-build_lib = os.path.join(build_base, platform_lib_name)
+TESTDATA_DIR = os.path.join(project_dir, 'tests', 'testdata')
 
-# prepend build/lib to PYTHONPATH environment variable
+# Get the platform/version-specific build folder.
+# By default, the distutils build base is in the same location as setup.py.
+platform_lib_name = 'lib.{platform}-{version[0]}.{version[1]}'.format(
+    platform=sysconfig.get_platform(), version=sys.version_info)
+build_dir = os.path.join(project_dir, 'bin', platform_lib_name)
+
+# Prepend the build folder to sys.path and the PYTHONPATH environment variable.
+if build_dir not in sys.path:
+    sys.path.insert(0, build_dir)
 TEST_ENV = os.environ.copy()
 if 'PYTHONPATH' not in TEST_ENV:
-    TEST_ENV['PYTHONPATH'] = build_lib
+    TEST_ENV['PYTHONPATH'] = build_dir
 else:
-    TEST_ENV['PYTHONPATH'] = build_lib + os.pathsep + TEST_ENV['PYTHONPATH']
+    TEST_ENV['PYTHONPATH'] = build_dir + os.pathsep + TEST_ENV['PYTHONPATH']
