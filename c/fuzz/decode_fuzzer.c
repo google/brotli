bc9e7d04f130f8521ced949a59ee268492a124fc
kasper <33230602+kasper3@users.noreply.github.com>
2018-06-10 15:23:17 +0000

    Convert fuzz/*.cc to C

--- c/fuzz/decode_fuzzer.c
+++ c/fuzz/decode_fuzzer.c
@@ -4,18 +4,19 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #include <brotli/decode.h>
 
 // Entry point for LibFuzzer.
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   size_t addend = 0;
   if (size > 0)
     addend = data[size - 1] & 7;
   const uint8_t* next_in = data;
 
   const int kBufferSize = 1024;
-  uint8_t* buffer = new uint8_t[kBufferSize];
+  uint8_t* buffer = (uint8_t*)malloc(sizeof(uint8_t) * kBufferSize);
   /* The biggest "magic number" in brotli is 16MiB - 16, so no need to check
      the cases with much longer output. */
   const size_t total_out_limit = (addend == 0) ? (1 << 26) : (1 << 24);
@@ -48,6 +49,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   }
 
   BrotliDecoderDestroyInstance(state);
-  delete[] buffer;
+  free(buffer);
   return 0;
 }
