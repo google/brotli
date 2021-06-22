6d66b68d9cc69891447119cbb22d2091c611285b
Adam Young <ayoung@redhat.com>
2021-06-22 11:19:03 -0400

    Update buffer parameter types in encode and decode functions

--- c/dec/decode.c
+++ c/dec/decode.c
@@ -2032,9 +2032,12 @@ static BROTLI_NOINLINE BrotliDecoderErrorCode SafeProcessCommands(
   return ProcessCommandsInternal(1, s);
 }
 
-BrotliDecoderResult BrotliDecoderDecompress(
-    size_t encoded_size, const uint8_t* encoded_buffer, size_t* decoded_size,
-    uint8_t* decoded_buffer) {
+
+BROTLI_DEC_API BrotliDecoderResult BrotliDecoderDecompress(
+    size_t encoded_size,
+    const uint8_t encoded_buffer[BROTLI_ARRAY_PARAM(encoded_size)],
+    size_t* decoded_size,
+    uint8_t decoded_buffer[BROTLI_ARRAY_PARAM(*decoded_size)]){
   BrotliDecoderState s;
   BrotliDecoderResult result;
   size_t total_out = 0;
