30c7d2f97dd09566874e7f9a518e7ca0577e8256
Evgenii Kliuchnikov <eustas@google.com>
2025-09-01 03:23:29 -0700

    split prefix.h to .h/.cc/_inc.h
    PiperOrigin-RevId: 801742168

---
+++ c/dec/prefix.c
@@ -0,0 +1,20 @@
+/* Copyright 2025 Google Inc. All Rights Reserved.
+
+   Distributed under MIT license.
+   See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+*/
+
+#include "prefix.h"
+
+#include "../common/platform.h"  /* IWYU pragma: keep */
+
+#if defined(__cplusplus) || defined(c_plusplus)
+extern "C" {
+#endif
+
+/* Embed kCmdLut. */
+#include "prefix_inc.h"
+
+#if defined(__cplusplus) || defined(c_plusplus)
+}  /* extern "C" */
+#endif
